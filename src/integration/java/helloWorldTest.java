aebafae5519f95237d0f5c254a7e7117d2b684cd
Carl Hostetler <carl.hostetler@gmail.com>
2015-11-23 21:52:41 -0800

    Add simple endpoint test using restassured
    * Add simple restassured service test
    * update gradle wraper properties to fix intellij hylighting

---
+++ src/integration/java/helloWorldTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class helloWorldTest {
+    @Test
+    public void testGet() {
+        when().get("http://localhost:8080/hello").then().statusCode(200);
+    }
+}
